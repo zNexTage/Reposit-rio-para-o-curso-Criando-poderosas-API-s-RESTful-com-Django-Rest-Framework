591e842c53233296433c5551160e81b0415330d8
christian_oliveira <christianoliveira1908@hotmail.com>
2021-09-30 21:24:59 -0300

    Consultas na api com filtragem.

--- atracoes/api/viewset.py
+++ atracoes/api/viewset.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from atracoes.models import Atracoes
 from .serializer import AtracoesSerializer
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class AtracoesViewSet(ModelViewSet):
     queryset = Atracoes.objects.all()
     serializer_class = AtracoesSerializer
+    filter_backends=(DjangoFilterBackend,)
+    filter_fields=('nome', 'descricao')
+
--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
@@ -9,10 +10,25 @@ class PontoTuristicoViewSet(ModelViewSet):
     # queryset=PontoTuristico.objects.all()
     serializer_class=PontoTuristicoSerializer
     # http_method_names=['DELETE', ] # Quais requisições http a nossa api aceita
-
+    filter_backends=(SearchFilter,)
+    search_fields=('nome', 'descricao', 'endereco__linha1')
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset= queryset.filter(nome__iexact=nome)
+        
+        if descricao:
+            queryset= queryset.filter(descricao__iexact=descricao)
+
+        return queryset
         
     # Override das funções padrões da Viewset
     # O objeto request retorna diversas informações, por exemplo: usuário logado, paramêtros e etc...
