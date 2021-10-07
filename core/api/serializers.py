39c1abaef94703c9f0f0bac066e8f33b9aed6496
christian_oliveira <christianoliveira1908@hotmail.com>
2021-10-07 20:43:11 -0300

    Trabalhando com NestedSerializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,23 @@
+from rest_framework.fields import SerializerMethodField
 from core.models import PontoTuristico
 from rest_framework import serializers
-from core.models import PontoTuristico
+from atracoes.api.serializer import AtracoesSerializer
+from enderecos.api.serializer import EnderecoSerializer
+
 
 class PontoTuristicoSerializer(serializers.ModelSerializer):
+    atracoes = AtracoesSerializer(many=True)
+    endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField() #Executara a função get_descricao_completa
+    # Precisa colocar o campo descricao_completa no fields do serializer
     class Meta:
         model = PontoTuristico
-        fields=('id', 'nome', 'descricao', 'aprovado', 'fotos')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'fotos', 'atracoes',
+                  'comentarios', 'avaliacao', 'endereco', 'descricao_completa','descricao_completa_2') 
+        #descricao_completa_2 foi definido na model;
 
+    
+
+
+    def get_descricao_completa(self, obj):
+        return '{} - {}'.format(obj.nome, obj.descricao)
--- core/models.py
+++ core/models.py
@@ -5,6 +5,8 @@ from avaliacoes.models import Avaliacao
 from enderecos.models import Endereco
 
 # Create your models here.
+
+
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
@@ -12,9 +14,15 @@ class PontoTuristico(models.Model):
     atracoes = models.ManyToManyField(Atracoes)
     comentarios = models.ManyToManyField(Comentarios)
     avaliacao = models.ManyToManyField(Avaliacao)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE,null=True, blank=True)
-    fotos = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
+    endereco = models.ForeignKey(
+        Endereco, on_delete=models.CASCADE, null=True, blank=True)
+    fotos = models.ImageField(
+        upload_to='pontos_turisticos', null=True, blank=True)
+    
+    #Pode ser usado no serializer
+    @property
+    def descricao_completa_2(self):
+        return '{} - {} (Model)'.format(self.nome, self.descricao)
 
     def __str__(self):
         return self.nome
-    
