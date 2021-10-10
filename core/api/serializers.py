d8245040c1f6ba657ec7c0220d91a709ee69edac
christian_oliveira <christianoliveira1908@hotmail.com>
2021-10-10 13:48:05 -0300

    Trabalhando com ForeignKey

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,11 +4,12 @@ from core.models import PontoTuristico
 from rest_framework import serializers
 from atracoes.api.serializer import AtracoesSerializer
 from enderecos.api.serializer import EnderecoSerializer
+from enderecos.models import Endereco
 
 
 class PontoTuristicoSerializer(serializers.ModelSerializer):
     atracoes = AtracoesSerializer(many=True)
-    endereco = EnderecoSerializer(read_only=True)
+    endereco = EnderecoSerializer()
     # Executara a função get_descricao_completa
     descricao_completa = SerializerMethodField()
     # Precisa colocar o campo descricao_completa no fields do serializer
@@ -27,13 +28,17 @@ class PontoTuristicoSerializer(serializers.ModelSerializer):
             
             ponto.atracoes.add(at)
 
-    def create(self, validated_data):
-        print(validated_data)
+    def create(self, validated_data):        
         atracoes = validated_data.pop('atracoes')
+        endereco = validated_data.pop('endereco')
 
         ponto = PontoTuristico.objects.create(**validated_data)
+        end = Endereco.objects.create(**endereco)
 
         self.create_atracoes(atracoes, ponto)
+        ponto.endereco = end
+
+        ponto.save()
 
         return ponto
 
