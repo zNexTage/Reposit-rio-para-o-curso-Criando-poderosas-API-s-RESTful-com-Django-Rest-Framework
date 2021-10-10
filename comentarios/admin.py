ebdfa111440e78ecc8bc16e7169adc6bef41d622
christian_oliveira <christianoliveira1908@hotmail.com>
2021-10-10 14:35:03 -0300

    Trabalhando com actions no admin

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Comentarios
+from .actions import reprova_comentarios, aprova_comentarios
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display=['usuario', 'data', 'aprovado']
+    actions = [reprova_comentarios, aprova_comentarios]
 
 # Register your models here.
-admin.site.register(Comentarios)
+admin.site.register(Comentarios, ComentarioAdmin)
+
+
+
